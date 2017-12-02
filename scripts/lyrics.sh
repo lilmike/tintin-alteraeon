1290c2c905a182d9e978051168438de10963050f
Storm Dragon <stormdragon2976@gmail.com>
2017-12-01 21:44:38 -0500

    Improvements to the lyrics script. This is still not the final installment.

--- scripts/lyrics.sh
+++ scripts/lyrics.sh
@@ -6,8 +6,9 @@ lyricsPath="${XDG_DATA_HOME:-$HOME/.local/share}/fortune"
 mkdir -p "$lyricsPath"
 
 # Get the song and artist info
-artist="$(echo "$2" | sed 's/\(.*\) - .*/\1/')"
-song="$(echo "$2" | sed 's/.* - \(.*\)/\1/')"
+artist="$(echo "$2" | cut -d '-' -f1)"
+song="$(echo "$2" | cut -d '-' -f2 | cut -d '(' -f1)"
+song=dd"$(echo "$2" | sed 's/.* - \(.*\)/\1/')"
 lyricsFile="${song// /-}.${artist// /-}"
 
 if ! [[ -f "$lyricsPath/$lyricsFile" ]]; then
@@ -21,12 +22,15 @@ fi
 # Sometimes the file is created, but it's one long string
 # Try to protect against that since the one string will most likely be too long for the 400 character limit
 if ! grep '%' "$lyricsPath/$lyricsFile" &> /dev/null ; then 
-rm "$lyricsPath/$lyricsFile" "$lyricsPath/$lyricsFile.dat"
+rm "$lyricsPath/$lyricsFile.dat"
+tmp="$(fold -s "$lyricsPath/$lyricsFile" | sed '0~4 s/$/\n%/g')"
+echo "$tmp" > "$lyricsPath/$lyricsFile"
+ strfile "$lyricsPath/$lyricsFile" &> /dev/null
 fi
 
 # Display the lyrics or an error if nothing is found
 if [[ -e "$lyricsPath/$lyricsFile" ]]; then
-echo "$1 $(fortune -n400 -s "$lyricsPath/$lyricsFile" | tr '[:space:]' ' ') -- it's $song by $artist"
+echo "$1 $(fortune -n400 -s -- "$lyricsPath/$lyricsFile" | tr '[:space:]' ' ') -- it's $song by $artist"
 else
 echo "#echo {No lyrics found for $song by $artist.}"
 fi
