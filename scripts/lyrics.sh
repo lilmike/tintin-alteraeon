b06aca9fb8b4f1c46cae0f7baf78e270a0121b25
Storm Dragon <stormdragon2976@gmail.com>
2017-12-02 17:58:24 -0500

    This is nearing the new release for the lyrics script.

--- scripts/lyrics.sh
+++ scripts/lyrics.sh
@@ -10,32 +10,33 @@ artist="$(echo "$2" | cut -d '-' -f1 | tr -s '[:space:]')"
 artist="${artist%[[:space:]]}"
 song="$(echo "$2" | cut -d '-' -f2 | cut -d '(' -f1 | tr -s '[:space:]')"
 song="${song%[[:space:]]}"
-song="$(echo "$2" | sed 's/.* - \(.*\)/\1/')"
 lyricsFile="${song// /-}.${artist// /-}"
 
+# Create a fortune file containing the lyrics
 if ! [[ -f "$lyricsPath/$lyricsFile" ]]; then
-clyrics "$song : $artist" > "$lyricsPath/$lyricsFile"
-sed -i 's/^$/%/g' "$lyricsPath/$lyricsFile"
- strfile "$lyricsPath/$lyricsFile" &> /dev/null
-# Remove the files if they don't contain anything
-[[ -s "$lyricsPath/$lyricsFile" ]] || rm "$lyricsPath/$lyricsFile" "$lyricsPath/$lyricsFile.dat"
+  clyrics "$song : $artist" > "$lyricsPath/$lyricsFile"
+  sed -i 's/^$/%/g' "$lyricsPath/$lyricsFile"
+   strfile "$lyricsPath/$lyricsFile" &> /dev/null
+  # Remove the files if they don't contain anything
+  [[ -s "$lyricsPath/$lyricsFile" ]] || rm "$lyricsPath/$lyricsFile" "$lyricsPath/$lyricsFile.dat"
 fi
 
 # Sometimes the file is created, but it's one long string
 # Try to protect against that since the one string will most likely be too long for the 400 character limit
 if [[ -s "$lyricsPath/$lyricsFile" ]]; then
-if ! grep '%' "$lyricsPath/$lyricsFile" &> /dev/null ; then 
-rm "$lyricsPath/$lyricsFile.dat"
-tmp="$(fold -s "$lyricsPath/$lyricsFile" | sed '0~4 s/$/\n%/g')"
-echo "$tmp" > "$lyricsPath/$lyricsFile"
- strfile "$lyricsPath/$lyricsFile" &> /dev/null
-fi
+  if ! grep '%' "$lyricsPath/$lyricsFile" &> /dev/null ; then 
+    rm "$lyricsPath/$lyricsFile.dat"
+    tmp="$(fold -s "$lyricsPath/$lyricsFile" | sed '0~4 s/$/\n%/g')"
+    echo "$tmp" > "$lyricsPath/$lyricsFile"
+     strfile "$lyricsPath/$lyricsFile" &> /dev/null
+  fi
 fi
 
 # Display the lyrics or an error if nothing is found
 if [[ -e "$lyricsPath/$lyricsFile" ]]; then
-echo "$1 $(fortune -n400 -s -- "$lyricsPath/$lyricsFile" | tr '[:space:]' ' ') -- it's $song by $artist"
+  echo "$1 $(fortune -n400 -s -- "$lyricsPath/$lyricsFile" | tr '[:space:]' ' ') -- it's $song by $artist"
 else
-echo "#echo {No lyrics found for $song by $artist.}"
+  echo "#echo {No lyrics found for $song by $artist.}"
 fi
+
 exit 0
